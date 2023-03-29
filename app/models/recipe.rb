5d28e07b9de87bb4584814812769791a1408af9c
Tracy Mumbi <tracychiuri@gmail.com>
2023-03-29 10:15:36 +0300

    Added recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
+    belongs_to :category
+    belongs_to :user
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :cooking_time, presence: true
 end
