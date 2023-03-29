55f680ae8adfe75063c02c4e5e43cbde2b80bbba
kennedy <kenneyrit@gmail.com>
2023-03-29 08:30:29 +0300

    migrate

---
+++ db/migrate/20230329051133_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.string :cooking_time
+
+      t.timestamps
+    end
+  end
+end
