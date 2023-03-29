1223b041d2282d3bc221a21edc1d711c627bc01a
Tracy Mumbi <tracychiuri@gmail.com>
2023-03-29 10:09:56 +0300

    Added categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,33 +1,40 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show update destroy ]
+  # before_action :set_category, only: %i[ show update destroy ]
 
   # GET /categories
   def index
     @categories = Category.all
-
     render json: @categories
   end
 
   # GET /categories/1
   def show
-    render json: @category
+    @category = Category.find_by(id: params[:id])
+    if @category
+      render json: @category, status: :ok
+    else
+      render json: {error: "Category not found"}, status: :not_found
+    end
   end
 
   # POST /categories
   def create
-    @category = Category.new(category_params)
+    @category = Category.create!(category_params)
 
-    if @category.save
-      render json: @category, status: :created, location: @category
+    if @category
+      render json: @category, status: :created
     else
-      render json: @category.errors, status: :unprocessable_entity
+    # rescue ActiveRecord::RecordInvalid => e
+      render json: {error: "Category not created"}
     end
   end
 
   # PATCH/PUT /categories/1
   def update
-    if @category.update(category_params)
-      render json: @category
+    @category = Category.find_by(id: params[:id])
+    if @category
+      @category.update!(category_params)
+      render json: @category, status: :ok
     else
       render json: @category.errors, status: :unprocessable_entity
     end
@@ -35,17 +42,18 @@ class CategoriesController < ApplicationController
 
   # DELETE /categories/1
   def destroy
+    @category = Category.find_by(id: params[:id])
     @category.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
+    # def set_category
+    #   @category = Category.find_by(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.require(:category).permit(:name, :origin, :image, :description)
+      params.permit(:name, :origin, :image, :description)
     end
 end
