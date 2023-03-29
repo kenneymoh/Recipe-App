5b8550d1e1113fda6fdae0256b35b94dc84dc754
Tracy Mumbi <tracychiuri@gmail.com>
2023-03-29 10:12:40 +0300

    Added recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,48 +1,54 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[ show update destroy ]
+  # before_action :set_recipe, only: %i[ show update destroy ]
 
   # GET /recipes
   def index
     @recipes = Recipe.all
-
     render json: @recipes
   end
 
   # GET /recipes/1
   def show
-    render json: @recipe
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      render json: @recipe, status: :ok
+    else
+      render json: {error: "recipe not found"}, status: :not_found
+    end
   end
 
   # POST /recipes
   def create
-    @recipe = Recipe.new(recipe_params)
-
-    if @recipe.save
-      render json: @recipe, status: :created, location: @recipe
+    @recipe = Recipe.create!(recipe_params)
+    if @recipe
+      render json: @recipe, status: :created
     else
-      render json: @recipe.errors, status: :unprocessable_entity
+      render json: {error: "recipe not created"}
     end
   end
 
   # PATCH/PUT /recipes/1
   def update
-    if @recipe.update(recipe_params)
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      @recipe.update!(recipe_params)
       render json: @recipe
     else
-      render json: @recipe.errors, status: :unprocessable_entity
+      render json: {error: "Not updated succefully"}
     end
   end
 
   # DELETE /recipes/1
   def destroy
+    @recipe = Recipe.find_by(id: params[:id])
     @recipe.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
+    # def set_recipe
+    #   @recipe = Recipe.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def recipe_params
