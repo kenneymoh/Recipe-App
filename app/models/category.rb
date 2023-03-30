48800fe6f5e788c1e971648e29b63a1943320067
phylis13 <phylisnjeri54@gmail.com>
2023-03-30 21:46:57 +0300

    update

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,2 @@
 class Category < ApplicationRecord
-<<<<<<< HEAD
-    has_many :recipes
-
-    validates :name, presence: true
-    validates :description, presence: true
-    validates :origin, presence: true
-=======
-
->>>>>>> db80fa1 (Added seed file)
 end
