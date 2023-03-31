4bad2318e8e64ed4fc058443ba630268ffd45a05
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-03-31 13:40:22 +0300

    updated category

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 function Category() {
-    const [category, setCategory] = useState([])
-
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => response.json())
-    .then(data => {
-      // console.log(data)
-      setCategory(data)
-    }
-      )
-
+    const [category, setCategories] = useState([])
+    useEffect(() => {
+      fetch("/categories")
+      .then(response => response.json())
+      .then(data => {
+          // console.log(data)
+          setCategories(data)
+      })
+    }, [])
   return (
+
     <div>
-        {category.map(cat => {
-          
-        })}
+        {category.map((cat) => 
+                <div>
+                    <h2>{cat.name}</h2>
+                    <p>{cat.description}</p>
+                    <img src={cat.image}/>
+                </div>
+            
+        )}
     </div>
   )
 }
