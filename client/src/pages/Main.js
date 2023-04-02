a49fcf42a9579a23d5f4ee918b0d30c1725c78bf
kennedy <kenneyrit@gmail.com>
2023-04-02 15:22:53 +0300

    done

---
+++ client/src/pages/Main.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Navbar from '../components/commons/Navbar'
+import Home from './Home.js'
+import Footer from '../components/commons/Footer'
+import Login from '../components/Login'
+import { Routes, Route} from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
+import Register from '../components/Register'
+// import Landing from '../components/Landing'
+import AddRecipe from '../components/AddRecipe'
+
+function Main() {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+      <Route
+        exact
+        path = '/home'
+        element = {<Home />}
+        />
+        <Route
+        exact
+        path = '/register'
+        element = {<Register />}
+        />
+        <Route
+        exact
+        path = '/login'
+        element = {<Login />}
+        />
+        <Route
+        exact
+        path = '/addcategory'
+        element = {<AddRecipe />}
+        />
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Main
